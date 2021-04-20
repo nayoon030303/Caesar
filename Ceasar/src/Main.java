f993da031f2c4197648b1eb9d6cefaa7813bbe43
nayoon030303 <nayoon030303@gmail.com>
2021-04-21 01:23:29 +0900

    catch depulication error

--- Ceasar/src/Main.java
+++ Ceasar/src/Main.java
@@ -9,31 +9,30 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("암호키: ");
-		String str = scan.next();
-		System.out.println(str);
+		String key = scan.nextLine();
+		System.out.print("입력 문장: ");
+		String str = scan.nextLine();
+		//System.out.println(key);
+		//System.out.println(str);
 		
 		/*중복 문자 제거*/
-		String delStr = deDuplication(str);
+		String delStr = deDuplication(key);
 		
 		/*Board 만들기*/
 		Vector<String> pwBoard = makePasswordBoard(delStr);
 		
 		
 	}
-	/*중복 문자 제거 함수*/
-	public static String deDuplication(String str) {
-		String delStr = str.toUpperCase();
-		
-		for(int i=0; i<delStr.length()-1; i++) {
-			String current = Character.toString(delStr.charAt(i));
-			String next = Character.toString(delStr.charAt(i+1));
-			if(current.equals(next)) {
-				delStr = delStr.replaceFirst(current, "");
-				i = -1;
-				//System.out.println(current+", "+next);
+	/*key 중복 문자 제거 함수*/
+	public static String deDuplication(String key) {
+		key = key.toUpperCase();
+		String delStr = "";
+		for(int i=0; i<key.length()-1; i++) {
+			String current = Character.toString(key.charAt(i)); //char->String
+			if(!delStr.contains(current)){
+				delStr+=current+"";
 			}
 		}
-		System.out.println(delStr);
 		return delStr;
 	}
 
@@ -49,6 +48,7 @@ public class Main {
 		for(int i=str.length(); i<26; i++) {
 			String ch = Character.toString((char)j);
 			if(!str.contains(ch)){//암호키에 들어있지 않다면
+				System.out.println(ch);
 				pwBoard.add(ch);
 			}else {
 				i--;
@@ -74,6 +74,11 @@ public class Main {
 		} 
 		
 		return pwBoard;
+		
+	}
+
+	/*암호화하기*/
+	public static void encryption(String str) {
 		
 	}
 }
