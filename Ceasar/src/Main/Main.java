00765f1793a7cda0c099704d4fecce35cd8a0525
nayoon030303 <nayoon030303@gmail.com>
2021-04-21 11:33:27 +0900

    check blanck/"Z"

--- Ceasar/src/Main/Main.java
+++ Ceasar/src/Main/Main.java
@@ -1,3 +1,4 @@
+package Main;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -8,21 +9,50 @@ public class Main {
 		//TitlePage t = new TitlePage();
 		Scanner scan = new Scanner(System.in);
 		
+		String blankCheck="";
+		String zCheck ="";
+		String key ="";
+		String input ="";
+		
 		System.out.print("암호키: ");
-		String key = scan.nextLine();
+		key = scan.nextLine();
 		System.out.print("입력 문장: ");
-		String str = scan.nextLine();
+		input = scan.nextLine();
+		
+		
 		//System.out.println(key);
 		//System.out.println(str);
 		
-		/*중복 문자 제거*/
+		/*key 중복 문자 제거*/
 		String delStr = deDuplication(key);
 		
 		/*Board 만들기*/
 		Vector<String> pwBoard = makePasswordBoard(delStr);
 		
-		
+		//문자 변환
+		input= input.toUpperCase();
+		for(int i=0; i<input.length(); i++) {
+			if(input.charAt(i) ==' ') {
+				input = input.substring(0,i)+input.substring(i+1,input.length());
+				blankCheck+="10";
+			}else {
+				blankCheck+="0";
+			}
+			
+			if(input.charAt(i)=='Z') {
+				input = input.substring(0,i)+"Q"+input.substring(i+1,input.length());
+				zCheck+="1";
+			}else {
+				zCheck+="0";
+			}
+		}
+		System.out.println();
+		System.out.println(input);
+		System.out.println(zCheck);
+		System.out.println(blankCheck);
+		//Vector<String>  = makeTwoLetters(input);
 	}
+	
 	/*key 중복 문자 제거 함수*/
 	public static String deDuplication(String key) {
 		key = key.toUpperCase();
@@ -48,7 +78,6 @@ public class Main {
 		for(int i=str.length(); i<26; i++) {
 			String ch = Character.toString((char)j);
 			if(!str.contains(ch)){//암호키에 들어있지 않다면
-				System.out.println(ch);
 				pwBoard.add(ch);
 			}else {
 				i--;
@@ -77,8 +106,41 @@ public class Main {
 		
 	}
 
-	/*암호화하기*/
-	public static void encryption(String str) {
+	/*2글짜씩 만들기*/
+	public static Vector<String> makeTwoLetters(String input) {
+		//String[] str = input.replace(" ", "").toUpperCase().split("");
 		
+		
+		
+		Vector<String> vectorStr = new Vector<String>();
+		
+		/*for(int i=0; i<str.length; i+=2) {
+			String cur = str[i];
+			String next =null;
+			if(i<str.length-1) {
+				next = str[i+1];
+			}else {
+				next = "X";
+			}
+			
+			if(cur.equals(next)) {
+				vectorStr.add(cur);
+				vectorStr.add("X");
+				i-=1;
+				//vectorStr.add(next);
+			}else {
+				vectorStr.add(cur);
+				vectorStr.add(next);
+			}
+		}
+		System.out.println();
+		
+		for(int i=0; i<vectorStr.size(); i++) {
+			if(i%2==0)
+				System.out.println();
+			System.out.print(vectorStr.get(i)+" ");
+		}*/
+		
+		return vectorStr;
 	}
 }
